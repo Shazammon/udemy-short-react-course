7edf7d6772b697a249d03f13612af6982f2c8ffc
Shazammon <ammonorgill@gmail.com>
2023-03-07 16:40:10 -0700

    updated props passed to Todo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo />
-      <Todo />
-      <Todo />
+      <Todo text='Learn React'/>
+      <Todo text='Update Todo'/>
+      <Todo text='Finish tutorial'/>
     </div>
   );
 }
