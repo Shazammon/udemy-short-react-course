70f532dac1240b4cfa57b0f5b2294d43e475c6dc
Shazammon <ammonorgill@gmail.com>
2023-03-07 17:00:44 -0700

    updated Modal return function

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,12 @@
 function Modal(props) {
 
+    return (
+        <div>
+            <p>Are you sure?</p>
+            <button className="btn btn--alt">Cancel</button>
+            <button className="btn">Confirm</button>
+        </div>
+    )
 
 }
 
